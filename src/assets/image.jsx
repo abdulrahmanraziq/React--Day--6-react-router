b1e748f02dc7c20febe3e911fac1acfbc9a29bbd
abdulrahmanraziq <abdulrahmanraziq548@gmail.com>
2024-06-03 20:09:30 +0530

    images added

--- src/assets/image.jsx
+++ src/assets/image.jsx
@@ -1,59 +1,59 @@
 export const bestFullStackDevelopment =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/07/person-front-computer-working-html.jpg";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717421729/abusnywtlwbrxaby5hku.jpg";
 
 export const fullStackDevelopment2 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-2048x1072.webp";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717422662/how-long-it-would-it-take-to-learn-full-stack-development_-2048x1072_unvfu9.webp";
 
 export const fullStackDevelopment3 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/02/1_6rDcIgFJQldloIERiUSmzw.png";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717422760/1_6rDcIgFJQldloIERiUSmzw_dxttca.webp";
 
 export const fullStackDevelopment4 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/02/DBMS1.png";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717422825/DBMS1_wq07ge.webp";
 
 export const fullStackDevelopment5 =
-  "https://www.guvi.in/blog/wp-content/uploads/2021/09/Full-Stack-Developer-course-syllabus.png";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717422920/Full-Stack-Developer-course-syllabus_qs5y6c.webp";
 
 export const fullStackDevelopment6 =
-  "https://www.guvi.in/blog/wp-content/uploads/2021/05/react-768x402.jpg";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423085/react-768x402_xpw1ce.jpg";
 
 export const dataScience1 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science.webp";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423201/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science_rll0u7.webp";
 
 export const dataScience2 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/07/Real-World-Data-Science-Examples-2048x1072.webp";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423292/Real-World-Data-Science-Examples-2048x1072_i8fjnk.webp";
 
 export const dataScience3 =
-  "https://www.guvi.in/blog/wp-content/uploads/2022/06/5.-data-science-2048x1072.png";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423441/5_rdlcjb.webp";
 
 export const dataScience4 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-Top-Product-Based-Companies-for-Data-Scientists.webp";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423513/Feature-image-Top-Product-Based-Companies-for-Data-Scientists_qjfraf.webp";
 
 export const dataScience5 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/07/image-4.png";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423580/image-4_bjgabb.webp";
 
 export const dataScience6 =
-  "https://www.guvi.in/blog/wp-content/uploads/2023/08/How-Long-Would-It-Take-to-Learn-Data-Science-2048x1072.png";
+  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423658/How-Long-Would-It-Take-to-Learn-Data-Science-2048x1072_nxum5t.webp";
 
-export const cyberSecurity1 = "https://www.guvi.in/blog/wp-content/uploads/2022/12/Yellow-Blue-Illustration-Business-Blog-Banner-2048x1152.png"
+export const cyberSecurity1 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423842/Yellow-Blue-Illustration-Business-Blog-Banner-2048x1152_eupsno.webp"
 
-export const cybersecuity2 = "https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Non-Coding-Jobs-in-Cybersecurity.webp"
+export const cybersecuity2 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717423973/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science_irthjp.webp"
 
-export const cybersecuity3 = "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp"
+export const cybersecuity3 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717424161/Feature-image-What-Is-Hacking_-Types-of-Hacking-More_xwbgyn.webp"
 
-export const cybersecuity4 = "https://www.guvi.in/blog/wp-content/uploads/2022/11/Teal-Illustration-Digital-Business-Blog-Banner-1-2048x1152.png"
+export const cybersecuity4 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717424265/Teal-Illustration-Digital-Business-Blog-Banner-1-2048x1152_e5tql2.webp"
 
-export const cybersecuity5 = "https://www.guvi.in/blog/wp-content/uploads/2022/12/cybersecuity-.gif"
+export const cybersecuity5 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717424352/cybersecuity-_h8qxrk.gif"
 
-export const cybersecuity6 = "https://www.guvi.in/blog/wp-content/uploads/2023/11/Top-10-Non-Coding-Jobs-in-DevOps.png"
+export const cybersecuity6 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717424477/Top-10-Non-Coding-Jobs-in-DevOps_w5uv4b.webp"
 
-export const deveops1 = "https://www.guvi.in/blog/wp-content/uploads/2023/07/DevOps-Project-Ideas-2048x1072.webp"
+export const deveops1 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717424614/DevOps-Project-Ideas-2048x1072_swbkfv.webp"
 
-export const deveops2 = "https://www.guvi.in/blog/wp-content/uploads/2023/07/feature-image-Best-YouTube-Channels-to-Learn-DevOps-from-Scratch.jpg"
+export const deveops2 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717424738/feature-image-Best-YouTube-Channels-to-Learn-DevOps-from-Scratch_kakqf2.jpg"
 
-export const deveops3 = "https://www.guvi.in/blog/wp-content/uploads/2023/08/Featured-image-Best-Way-to-Learn-DevOps-2048x1072.webp"
+export const deveops3 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717425023/Featured-image-Best-Way-to-Learn-DevOps-2048x1072_teqcjz.webp"
 
-export const deveops4 =  "https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-Product-Based-Companies-for-DevOps-Engineers-in-India.webp"
+export const deveops4 =  "https://res.cloudinary.com/dyl0reuky/image/upload/v1717425152/Feature-image-Top-Product-Based-Companies-for-DevOps-Engineers-in-India_ysfpm3.webp"
 
-export const deveops5 = "https://www.guvi.in/blog/wp-content/uploads/2023/05/2202.i402.018.S.m004.c13.Devops-engineer-flat-composition-2048x1575.jpg"
+export const deveops5 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717425313/2202.i402.018.S.m004.c13.Devops-engineer-flat-composition-2048x1575_ufl339.jpg"
 
-export const deveops6 = "https://www.guvi.in/blog/wp-content/uploads/2023/11/Top-10-Non-Coding-Jobs-in-DevOps.png"
+export const deveops6 = "https://res.cloudinary.com/dyl0reuky/image/upload/v1717425497/Top-10-Non-Coding-Jobs-in-DevOps_jub3wh.webp"
